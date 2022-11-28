eca1b7b2c040fd74bafd4b85a43ad7e2fdf94431
lucapinciroli <luca.pinciroli02@gmail.com>
2022-11-28 17:13:32 +0100

    fix del precedente commit

--- Prolog/jsonparse.pl
+++ Prolog/jsonparse.pl
@@ -10,22 +10,19 @@
 
 
 % trasformo subito la stringa in una lista di codici di caratteri
-% jsonparse/2 chiama direttamente jsonparse/5 sulla lista di codici di caratteri
-
 % quanti e quali argomenti bisogna avere per poter parsare? 5 come gli interi?
-
 jsonparse(JSONString, Object) :-
     string_codes(JSONString, ListOfCodes),
-    jsonparse(ListOfCodes, Accumulator, OtherCodes, Elements, Object). % (?)
+    jsonparse(ListOfCodes, Object). % (?)
 
-jsonparse(JSONString, Object) :-
+jsonparse(ListOfCodes, Object) :-
     % predicato per controllare se JSONString è una stringa
 
-jsonparse(JSONString, Object) :-
+jsonparse(ListOfCodes, Object) :-
     % predicato per controllare se JSONString è un numero
 
-jsonparse(JSONString, Object) :-
+jsonparse(ListOfCodes, Object) :-
     % predicato per controllare se JSONString è un oggetto JSON
 
-jsonparse(JSONString, Object) :-
+jsonparse(ListOfCodes, Object) :-
     % predicato per controllare se JSONString è un array JSON
