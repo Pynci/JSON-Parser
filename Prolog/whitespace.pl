f2dce8460684a64c6bc84684594e48a17030d474
lucapinciroli <luca.pinciroli02@gmail.com>
2022-12-07 13:26:59 +0100

    trim_testa/2 funziona ma trim_coda/2 va ancora fixato. Implementato reverse/2 per fare da supporto

--- Prolog/whitespace.pl
+++ Prolog/whitespace.pl
@@ -3,9 +3,10 @@
 trim(Atomo, ListaTrimmata) :-
     atom(Atomo),
     atom_chars(Atomo, ListaCaratteri),
-    trim_testa(ListaCaratteri, ListaTrimmata).
+    trim_testa(ListaCaratteri, TrimmataTesta),
+    trim_coda(TrimmataTesta, ListaTrimmata).
 
-
+%%% -- Implementazione trim_testa/2 e trim_coda/2 --
 
 %%% trim_testa/2 rimuove le spaziature superflue in testa
 
@@ -27,9 +28,25 @@ trim_testa([Spazio, Carattere | Altro], [Spazio, Carattere | Altro]) :-
     not(char_type(Carattere, space)).
 
 
-%%% trim_coda/2 rimuove le spaziature superflue in coda
-% continua
+%%% DA FIXARE: trim_coda, così fa loop strani
 
+%%% trim_coda/2 rimuove le spaziature superflue in coda
+trim_coda(ListaDaTrimmare, ListaTrimmata) :-
+    reverse(ListaDaTrimmare, ListaRibaltata),
+    !,
+    trim_testa(ListaRibaltata, TrimmataTesta),
+    reverse(TrimmataTesta, ListaTrimmata).
+
+%%% reverse/2 inverte una lista
+reverse(Lista, ListaInvertita) :-
+    reverse(Lista, _Accumulatore, ListaInvertita).
+
+%%% reverse/3 fa da supporto a reverse/2
+reverse([], ListaInvertita, ListaInvertita).
+reverse([Carattere | Resto], Accumulatore, ListaInvertita) :-
+    reverse(Resto, [Carattere | Accumulatore], ListaInvertita).
+
+%%% -- Fine implementazione trim_testa/2 e trim_coda/2 --
 
 
 
