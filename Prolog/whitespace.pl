9c36a071c22e892d98feaac13d3cbf13b0682ca3
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-12-07 16:19:05 +0100

    Modifiche all'implementazione di trim/2 e trim_testa/2

--- Prolog/whitespace.pl
+++ Prolog/whitespace.pl
@@ -6,29 +6,33 @@ trim(Atomo, ListaTrimmata) :-
     trim_testa(ListaCaratteri, TrimmataTesta),
     trim_coda(TrimmataTesta, ListaTrimmata).
 
-%%% -- Implementazione trim_testa/2 e trim_coda/2 --
+
+%%% --- Implementazione trim_testa/2 e trim_coda/2 ---
 
 %%% trim_testa/2 rimuove le spaziature superflue in testa
+
+%%% se non ho caratteri il gioco finisce subito
+trim_testa([], []).
 
 %%% se il primo carattere non è di spaziatura sono a posto
 trim_testa([Carattere | Altro], [Carattere | Altro]) :-
     not(char_type(Carattere, space)),
     !.
 
-%%% se i primi 2 caratteri sono di spaziatura scarto il primo
-trim_testa([Spazio1, Spazio2 | Altro], NuovaLista) :-
-    char_type(Spazio1, space),
-    char_type(Spazio2, space),
-    !,
-    trim_testa([Spazio2 | Altro], NuovaLista).
+%%% caso base: non ci sono caratteri alfanumerici
+trim_testa([Spazio | []], []) :-
+    char_type(Spazio, space).
 
 %%% caso base: solo il primo primo carattere è di spaziatura
 trim_testa([Spazio, Carattere | Altro], [Spazio, Carattere | Altro]) :-
     char_type(Spazio, space),
     not(char_type(Carattere, space)).
 
-
-%%% DA FIXARE: trim_coda, così fa loop strani
+%%% se il primo carattere è di spaziatura lo scarto
+trim_testa([Spazio | Altro], NuovaLista) :-
+    char_type(Spazio, space),
+    !,
+    trim_testa(Altro, NuovaLista).
 
 %%% trim_coda/2 rimuove le spaziature superflue in coda
 trim_coda(ListaDaTrimmare, ListaTrimmata) :-
@@ -46,7 +50,7 @@ reverse([], ListaInvertita, ListaInvertita).
 reverse([Carattere | Resto], Accumulatore, ListaInvertita) :-
     reverse(Resto, [Carattere | Accumulatore], ListaInvertita).
 
-%%% -- Fine implementazione trim_testa/2 e trim_coda/2 --
+%%% --- Fine implementazione trim_testa/2 e trim_coda/2 ---
 
 
 
