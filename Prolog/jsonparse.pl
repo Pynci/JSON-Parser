773c62faeee7869be5ecbc58150b091cbb15d372
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-12-11 18:06:07 +0100

    "il commit importante è il prossimo" -Max Allegri

--- Prolog/jsonparse.pl
+++ Prolog/jsonparse.pl
@@ -7,20 +7,3 @@
 
 %% MOLTO IMPORTANTE: controllare di non superare le 80 colonne di larghezza
 %% e mettere SEMPRE gli "SPAZZZZI DOPO GLI OPERATOOHRI"
-
-is_spazio(' ').
-is_virgolette('\"').
-is_newline('\n').       %(?)
-is_quadra_aperta('[').
-is_quadra_chiusa(']').
-
-
-
-% trasformo subito la stringa in una lista di codici di caratteri
-% quanti e quali argomenti bisogna avere per poter parsare? 5 come gli interi?
-jsonparse(JSONString, Object) :-
-    atom_codes(JSONString, ListOfCodes),
-    jsonparse(ListOfCodes, Object).
-
-jsonparse([X | Xs], Object) :-
-    %jsonarray...
