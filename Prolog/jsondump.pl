47e58069ae6f08988199e509e0b8f535d12ea618
Marco <m.ferioli@campus.unimib.it>
2022-12-20 17:29:41 +0100

    Aggiunto file con funzione jsondump.

--- Prolog/jsondump.pl
+++ Prolog/jsondump.pl
@@ -1,1 +1,10 @@
 %%% -*- Mode: Prolog -*-
+
+jsondump(JSON, FileName) :-
+    inverti(JSON, JSONInvertito),
+    open(FileName, write, Out),
+    write(Out, JSONInvertito),
+    put(Out, '.'),
+    nl(Out),
+    close(Out).
+    
