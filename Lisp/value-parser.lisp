8095e7ab2e2e1f5e46f4d36f26a3572933296469
Marco <m.ferioli@campus.unimib.it>
2022-12-27 10:47:13 +0100

    Implementata la possibilità di effettuare il parsing di numeri interi, negativi, in Q, delle parole chiavi null, true, false. Il parser value ritorna sempre una cons contenente come car il valore parsato e come cdr il resto della stringa.

--- Lisp/value-parser.lisp
+++ Lisp/value-parser.lisp
@@ -2,10 +2,26 @@
 
 ; sdp = stringa da parsare
 ; sl = stringa letta
-(defun value-parser (sdp)
-  (cond ((equal (subseq sdp 0 1) "\"") (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
-    (cons sl (subseq sdp (length sl) (length sdp)))))))
+;numlet = numero letto
 
+(defun value-parser (sdp)
+  (cond ((zerop (length sdp)) (print "Non esiste nulla da analizzare."))
+        ((equal (subseq sdp 0 1) "\"") 
+         (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
+           (cons sl (subseq sdp (length sl)))))
+        ((numberp (digit-char-p (char sdp 0))) 
+         (let ((numlet (leggi-numero sdp)))
+           (cons (parse-float numlet) (subseq sdp (length numlet)))))
+        ((equal (subseq sdp 0 1) "-") 
+         (let ((numlet (concatenate 'string "-" (leggi-numero (subseq sdp 1)))))
+           (cons (parse-float numlet) (subseq sdp (length numlet)))))
+        ((equal (subseq sdp 0 4) "null") 
+         (cons "null" (subseq sdp 4)))
+        ((equal (subseq sdp 0 4) "true") 
+         (cons "true" (subseq sdp 4)))
+        ((equal (subseq sdp 0 5) "false") 
+         (cons "false" (subseq sdp 5)))
+        ))
 ;;; Fine funzione value-parser
 
 ;;; Funzione leggi-stringa
@@ -13,9 +29,23 @@
 (defun leggi-stringa (stringa)
   (if (equal (subseq stringa 0 1) "\"")
       "\""
-      (concatenate 'string (subseq stringa 0 1) (leggi-stringa (subseq stringa 1 (length stringa))))))
+    (concatenate 'string (subseq stringa 0 1) (leggi-stringa (subseq stringa 1 (length stringa))))))
 
 ;;; Fine funzione leggi-stringa
+
+;;; Funzione leggi-numero
+
+;pe = primo elemento
+
+(defun leggi-numero (stringa)
+  (let ((pe (subseq stringa 0 1)))
+    (if (not (numberp (digit-char-p (char pe 0))))
+        (if (not(equal pe "."))
+            ""
+            (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa)))))
+        (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa)))))))
+
+;;; Fine funzione leggi-numero
 
 
 
