20ac0b761420107540e319b640eb0931d6a8828a
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-12-29 22:02:12 +0100

    fixed #25

--- Lisp/value-parser.lisp
+++ Lisp/value-parser.lisp
@@ -7,22 +7,26 @@
 (defun value-parser (sdp)
   (cond ((zerop (length sdp)) (print "Non esiste nulla da analizzare."))
         ((equal (subseq sdp 0 1) "\"") 
-         (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
+          (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
            (cons sl (subseq sdp (length sl)))))
         ((numberp (digit-char-p (char sdp 0))) 
-         (let ((numlet (leggi-numero sdp)))
-           (cons (parse-float numlet) (subseq sdp (length numlet)))))
+          (let ((numlet (leggi-numero sdp)))
+            (cons (parse-float numlet) (subseq sdp (length numlet)))))
         ((equal (subseq sdp 0 1) "-") 
-         (let ((numlet (concatenate 'string "-" (leggi-numero (subseq sdp 1)))))
+          (let ((numlet (concatenate 'string "-" (leggi-numero (subseq sdp 1)))))
            (cons (parse-float numlet) (subseq sdp (length numlet)))))
-        ((equal (subseq sdp 0 4) "null") 
-         (cons "null" (subseq sdp 4)))
-        ((equal (subseq sdp 0 4) "true") 
-         (cons "true" (subseq sdp 4)))
-        ((equal (subseq sdp 0 5) "false") 
-         (cons "false" (subseq sdp 5)))
+        ((> (length sdp) 3)
+          (cond
+            ((equal (subseq sdp 0 4) "null") 
+              (cons "null" (subseq sdp 4)))
+            ((equal (subseq sdp 0 4) "true") 
+              (cons "true" (subseq sdp 4)))
+            ((equal (subseq sdp 0 5) "false") 
+              (cons "false" (subseq sdp 5)))
+            (T
+              (error "Input non valido"))))
         (T
-         (error "Input non valido"))
+          (error "Input non valido"))
         ))
 ;;; Fine funzione value-parser
 
@@ -49,13 +53,6 @@
               (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa)))))
           (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa))))))))
 
-
-#|
-(defun leggi-numero (stringa)
-  (let ((pe (subseq stringa 0 1)))
-    (if (equal pe ".")
-      )))
-|#
 
 ;;; Fine funzione leggi-numero
 
