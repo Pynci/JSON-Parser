75ee8851037b950de78b5603416188ee565b0558
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-12-29 23:51:44 +0100

    prima implementazione dell'array in lisp

--- Lisp/value-parser.lisp
+++ Lisp/value-parser.lisp
@@ -8,7 +8,7 @@
   (cond ((zerop (length sdp)) (print "Non esiste nulla da analizzare."))
         ((equal (subseq sdp 0 1) "[")
           (let ((array-letto (cons 'JSONARRAY (leggi-array (subseq sdp 1)))))
-            array-letto))
+            (cons (butlast array-letto) (last array-letto))))
         ((equal (subseq sdp 0 1) "\"") 
           (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
             (cons sl (subseq sdp (length sl)))))
@@ -60,17 +60,31 @@
 
 ;;; Funzione leggi-array
 
+#| SPIEGAZIONE "RAPIDA"
+Se la stringa in input è vuota allora restituisce la stringa vuota
+Altrimenti va a guardare il primo elemento (pe):
+  - se il primo elemento è ] allora restituisce (volutamente) una cons con dentro come car tutto il rimanente al di fuori
+    dell'array racchiuso in una stringa, e come cdr invece NIL (infatti sarà la cons che chiude la lista)
+  - se invece becca una virgola allora la salta e parsa ciò che si trova dopo la virgola e richiama la lettura dell'array
+    sui possibili elementi rimanenti (infatti se vedi restituisce una cons con car ciò che ha parsato value-parser e cdr quello
+    che manca da parsare per concludere l'array)
+  - se invece trova una qualsiasi altra cosa (che non sia ] o virgola) allora significa che ha davanti l'elemento da parsare
+Qual è il senso di restituire una lista del tipo (jsonarray elemento elemento ... robaFuoriArray) avente quindi come ultimo elemento
+una lista contenente tutto quello che rimane?
+In questo modo quando si torna indietro al value-parser è lui a occuparsi del resto: restituisce una cons con car tutta la lista escluso
+l'ultimo elemento (che è proprio la stringa contenente tutto il testo rimanente) e con cdr l'ultimo elemento rimosso prima.
+Se qualcosa non ti torna scrivimi e fammelo sapere! :D
+
+~Pynci
+ |#
 (defun leggi-array (stringa)
   (if (zerop (length stringa))
       ""
       (let ((pe (subseq stringa 0 1)))
         (cond
-          ((equal pe "]") NIL)
+          ((equal pe "]") (cons (subseq stringa 1 (length stringa)) NIL))
           ((equal pe ",")
             (let ((elementi-restanti (value-parser (subseq stringa 1 (length stringa)))))
               (cons (first elementi-restanti) (leggi-array (rest elementi-restanti)))))
           (T
             (cons (first (value-parser stringa)) (leggi-array (rest (value-parser stringa)))))))))
-
-
-;(cons (first (value-parser (subseq 1 (length stringa)))) (leggi-array (rest (value-parser stringa))))
