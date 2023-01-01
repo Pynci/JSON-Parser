e26ea852b3a19bf026c0942c6879a1f97670e1d0
Marco <m.ferioli@campus.unimib.it>
2023-01-01 17:25:12 +0100

    Commit per pigrizia

--- Lisp/value-parser.lisp
+++ Lisp/value-parser.lisp
@@ -61,23 +61,6 @@
 
 ;;; Funzione leggi-array
 
-#| SPIEGAZIONE "RAPIDA"
-Se la stringa in input è vuota allora restituisce la stringa vuota
-Altrimenti va a guardare il primo elemento (pe):
-  - se il primo elemento è ] allora restituisce (volutamente) una cons con dentro come car tutto il rimanente al di fuori
-    dell'array racchiuso in una stringa, e come cdr invece NIL (infatti sarà la cons che chiude la lista)
-  - se invece becca una virgola allora la salta e parsa ciò che si trova dopo la virgola e richiama la lettura dell'array
-    sui possibili elementi rimanenti (infatti se vedi restituisce una cons con car ciò che ha parsato value-parser e cdr quello
-    che manca da parsare per concludere l'array)
-  - se invece trova una qualsiasi altra cosa (che non sia ] o virgola) allora significa che ha davanti l'elemento da parsare
-Qual è il senso di restituire una lista del tipo (jsonarray elemento elemento ... robaFuoriArray) avente quindi come ultimo elemento
-una lista contenente tutto quello che rimane?
-In questo modo quando si torna indietro al value-parser è lui a occuparsi del resto: restituisce una cons con car tutta la lista escluso
-l'ultimo elemento (che è proprio la stringa contenente tutto il testo rimanente) e con cdr l'ultimo elemento rimosso prima.
-Se qualcosa non ti torna scrivimi e fammelo sapere! :D
-
-~Pynci
- |#
 (defun leggi-array (stringa-ricevuta)
   (let ((stringa (string-trim-whitespace stringa-ricevuta)))
     (if (zerop (length stringa))
@@ -96,5 +79,12 @@ Se qualcosa non ti torna scrivimi e fammelo sapere! :D
                       (cons (car primo-valore) (leggi-array (car (cdr primo-valore)))))
                     (t (cons (car primo-valore) (leggi-array (cdr primo-valore))))))))))))
 
+;;; Fine funzione leggi-array
 
-;[1,2, 3] [1,2] resto , 3
+;;; Funzione leggi-oggetto
+
+(defun leggi-oggetto (stringa-ricevuta)
+  (let ((stringa (string-trim-whitespace stringa-ricevuta)))
+    ))
+
+;;; Fine funzione leggi-oggetto
