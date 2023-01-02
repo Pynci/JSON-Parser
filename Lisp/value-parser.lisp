a49f837854a01b85389b6621555451097e76f098
Luca Pinciroli <luca.pinciroli02@gmail.com>
2023-01-02 17:13:28 +0100

    leggi-oggetto funziona con strutture base tipo stringhe e numeri

--- Lisp/value-parser.lisp
+++ Lisp/value-parser.lisp
@@ -1,25 +1,29 @@
-;;; Funzione value-parser
+
+(defun primo-carattere (stringa)
+  (subseq stringa 0 1))
+
+;;; Funzione parser-value
 
 ; sdp = stringa da parsare
 ; sl = stringa letta
 ;numlet = numero letto
 
-(defun value-parser (stringa-non-trimmata)
+(defun parser-value (stringa-non-trimmata)
   (let ((sdp (string-trim-whitespace stringa-non-trimmata)))
     (cond ((zerop (length sdp)) (print "Non esiste nulla da analizzare."))
-        ((equal (subseq sdp 0 1) "{")
+        ((equal (primo-carattere sdp) "{")
           (let ((oggetto-letto (cons 'jsonobj (leggi-oggetto (subseq sdp 1)))))
             (cons (butlast oggetto-letto) (last oggetto-letto))))
-        ((equal (subseq sdp 0 1) "[")
+        ((equal (primo-carattere sdp) "[")
           (let ((array-letto (cons 'jsonarray (leggi-array (subseq sdp 1)))))
             (cons (butlast array-letto) (last array-letto))))
-        ((equal (subseq sdp 0 1) "\"") 
+        ((equal (primo-carattere sdp) "\"") 
           (let ((sl (concatenate 'string "\"" (leggi-stringa (subseq sdp 1)))))
             (cons sl (subseq sdp (length sl)))))
         ((numberp (digit-char-p (char sdp 0))) 
           (let ((numlet (leggi-numero sdp)))
             (cons (parse-float numlet) (subseq sdp (length numlet)))))
-        ((equal (subseq sdp 0 1) "-") 
+        ((equal (primo-carattere sdp) "-") 
           (let ((numlet (concatenate 'string "-" (leggi-numero (subseq sdp 1)))))
            (cons (parse-float numlet) (subseq sdp (length numlet)))))
         ((> (length sdp) 3)
@@ -35,14 +39,14 @@
         (T
           (error "Input non valido"))
         )))
-;;; Fine funzione value-parser
+;;; Fine funzione parser-value
 
 ;;; Funzione parser-stringa
 
 (defun leggi-stringa (stringa)
-  (if (equal (subseq stringa 0 1) "\"")
+  (if (equal (primo-carattere stringa) "\"")
       "\""
-    (concatenate 'string (subseq stringa 0 1) (leggi-stringa (subseq stringa 1 (length stringa))))))
+    (concatenate 'string (primo-carattere stringa) (leggi-stringa (subseq stringa 1)))))
 
 ;;; Fine funzione leggi-stringa
 
@@ -53,31 +57,33 @@
 (defun leggi-numero (stringa)
   (if (zerop (length stringa))
     ""
-    (let ((pe (subseq stringa 0 1)))
+    (let ((pe (primo-carattere stringa)))
       (if (not (numberp (digit-char-p (char pe 0))))
           (if (not(equal pe "."))
               ""
-              (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa)))))
-          (concatenate 'string pe (leggi-numero (subseq stringa 1 (length stringa))))))))
+              (concatenate 'string pe (leggi-numero (subseq stringa 1))))
+          (concatenate 'string pe (leggi-numero (subseq stringa 1)))))))
 
 ;;; Fine funzione leggi-numero
 
 ;;; Funzione leggi-array
 
+
+;; REMINDER: rimanere coerenti con l'utilizzo di first/rest e di car/cdr
 (defun leggi-array (stringa-ricevuta)
   (let ((stringa (string-trim-whitespace stringa-ricevuta)))
     (if (zerop (length stringa))
       (error "leggi-array: hai fatto una cacata")
-      (let ((pe (subseq stringa 0 1)))
+      (let ((pe (primo-carattere stringa)))
         (cond
-          ((equal pe "]") (cons (subseq stringa 1 (length stringa)) NIL))
+          ((equal pe "]") (cons (subseq stringa 1) NIL))
           ((equal pe ",")
-            (let ((elementi-restanti (value-parser (subseq stringa 1 (length stringa)))))
+            (let ((elementi-restanti (parser-value (subseq stringa 1))))
               (cond ((listp (car elementi-restanti))
                       (cons (first elementi-restanti) (leggi-array (car (cdr elementi-restanti)))))
                     (t (cons (first elementi-restanti) (leggi-array (rest elementi-restanti)))))))
           (T
-            (let ((primo-valore (value-parser stringa)))
+            (let ((primo-valore (parser-value stringa)))
               (cond ((listp (car primo-valore)) 
                       (cons (car primo-valore) (leggi-array (car (cdr primo-valore)))))
                     (t (cons (car primo-valore) (leggi-array (cdr primo-valore))))))))))))
@@ -89,15 +95,30 @@
 (defun leggi-oggetto (stringa-ricevuta)
   (let ((stringa (string-trim-whitespace stringa-ricevuta)))
     (if (zerop (length stringa))
-        (error "leggi-oggetto: hai fatto una cacata")
-        (let ((pe (subseq stringa 0 1)))
-          (if (equal pe "}")
-              (cons (subseq stringa 1 (length stringa)) NIL)
-              leggi-coppia(stringa))))))
+        (error "jsonparse: syntax error (invalid object)")
+        (let ((pe (primo-carattere stringa)))
+          (cond 
+            ((equal pe "}") 
+              (cons (subseq stringa 1) NIL))
+            ((equal pe ",")
+              (let ((coppia (leggi-coppia (subseq stringa 1))))
+                (cons (car coppia) (leggi-oggetto (cdr coppia)))))
+            (T
+              (let ((coppia (leggi-coppia stringa)))
+                (cons (car coppia) (leggi-oggetto (cdr coppia))))))))))
 
 (defun leggi-coppia (stringa-ricevuta)
-  ; continua qui, me so cacato er cazzo dato che sono le 19:30
-  )
+  (let ((stringa (string-trim-whitespace stringa-ricevuta)))
+    (if (equal (primo-carattere stringa) "\"")
+      (let  ((chiave (concatenate 'string "\"" (leggi-stringa (subseq stringa 1)))))
+        (let ((resto (string-trim-whitespace (subseq stringa (length chiave) (length stringa)))))
+          (if (equal (primo-carattere resto) ":")
+            (let ((valore (parser-value (string-trim-whitespace (subseq resto 1)))))
+              (cons (cons chiave (cons (car valore) NIL)) (cdr valore)))
+            (error "jsonparse: syntax error (missing ':')"))))
+      (error "jsonparse: syntax error (key is not a string)"))))
 ;;; Fine funzione leggi-oggetto 
 
 ; ((JSONARRAY 1 2 3) ",[4,5]]")
+
+; (cons (butlast oggetto-letto) (last oggetto-letto))
